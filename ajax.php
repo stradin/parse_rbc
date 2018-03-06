430e47f0420412c607fa73714a1d909485b1dad0
Vyacheslav Stradin <stradin.va@gmail.com>
2018-03-06 15:00:15 +0300

    delete case

--- ajax.php
+++ ajax.php
@@ -1,12 +1,7 @@
 <?php
 require_once ('helper.php');
 
-$task = $_POST['task'];
+$helper = new MyHelper();
+$result = $helper->ParseRBC();
+echo $result;
 
-switch ($task) {
-    case 'parser':
-        $helper = new MyHelper();
-        $result = $helper->ParseRBC();
-        echo $result;
-        break;
-}
